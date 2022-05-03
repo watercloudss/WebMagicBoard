ec96f3ad2cc14019fcf170b1aa20fb5ecbb76ec5
lly <1518189682@qq.com>
2022-05-03 23:51:06 +0800

    增加数据监控页面

---
+++ src/views/data/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <i-frame :src="url" />
+</template>
+<script>
+import iFrame from '@/components/iFrame/index'
+export default {
+  name: 'Druid',
+  components: { iFrame },
+  data() {
+    return {
+      url: 'http://127.0.0.1:9000/druid/login.html'
+    }
+  }
+}
+</script>
